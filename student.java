b7a7ec62b8e9104a7c22fe7f9dff64964f53dd86
arrunts491 <arrunts491@gmail.com>
2026-04-15 15:40:56 +0530

    Create student.java

---
+++ student.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class StudentGradeCalculator {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter marks for Subject 1: ");
+        int s1 = sc.nextInt();
+
+        System.out.print("Enter marks for Subject 2: ");
+        int s2 = sc.nextInt();
+
+        System.out.print("Enter marks for Subject 3: ");
+        int s3 = sc.nextInt();
+
+        int total = s1 + s2 + s3;
+        double average = total / 3.0;
+
+        System.out.println("Total Marks = " + total);
+        System.out.println("Average = " + average);
+
+        if (average >= 90) {
+            System.out.println("Grade: A");
+        } else if (average >= 75) {
+            System.out.println("Grade: B");
+        } else if (average >= 50) {
+            System.out.println("Grade: C");
+        } else {
+            System.out.println("Grade: Fail");
+        }
+
+        sc.close();
+    }
+}
